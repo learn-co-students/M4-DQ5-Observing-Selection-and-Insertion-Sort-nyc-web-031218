6e6b7c3942cd7cedf41e6c2a40bc7e0d3aa5bba2
victoria-huang <vctrah@gmail.com>
2018-05-18 10:17:34 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,21 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
+    let location = i - 1;
+
+    while (location >= 0) {
+      if (arr[location] > item) {
+        arr[location + 1] = arr[location];
+        location--;
+      } else {
+        break;
+      }
+    }
+
+    arr[location + 1] = item;
+  }
+
+  return arr;
 }
